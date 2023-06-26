81cf07e19b1ef7d1890cd53fe8d515f687e81c5d
niharikadhapola <47639810+niharikadhapola@users.noreply.github.com>
2023-06-26 04:08:43 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-sudo apt update
-sudo apt install libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+
+
+# Update package lists
+apt update
+
+# Install GStreamer development libraries and plugins
+apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+
 pip install -U pip
 pip install -r requirements.txt
 python3 virtual_keyboard.py
