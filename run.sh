ccd8c2b4ebd44c6eabdc336a8f7d4adc1ace4a87
niharikadhapola <47639810+niharikadhapola@users.noreply.github.com>
2023-07-08 13:53:08 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@
 apt update
 
 # Install GStreamer development libraries and plugins
-apt install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
+pip install -y libgstreamer1.0-dev libgstreamer-plugins-base1.0-dev
 
 pip install -U pip
 pip install -r requirements.txt
